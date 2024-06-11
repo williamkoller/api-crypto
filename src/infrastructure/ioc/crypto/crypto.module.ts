a23d9105bcc6fa4c4057cce9be27ab7140e9c934
William Koller <williamkoller30@gmail.com>
2024-06-11 19:48:01 -0300

    feat: add CachedModule

--- src/infrastructure/ioc/crypto/crypto.module.ts
+++ src/infrastructure/ioc/crypto/crypto.module.ts
@@ -3,9 +3,10 @@ import { CryptoController } from '@/controllers/crypto/crypto.controller';
 import { Binance } from '../../integrations/crypto/http/binance/binance';
 import { LoadTopCryptoUseCase } from '../../../usecases/load-top-crypto/load-top-crypto.usecase';
 import { HttpModule } from '@nestjs/axios';
+import { CachedModule } from '../cache/cached.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CachedModule],
   providers: [Binance, LoadTopCryptoUseCase],
   controllers: [CryptoController],
 })
